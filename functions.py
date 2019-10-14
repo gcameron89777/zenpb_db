e9b997cff926de7b084abfdd0002fdb28388561e
dougfir <4141685+gcameron89777@users.noreply.github.com>
2019-10-13 20:02:26 -0700

    Pull sessions from GA API V4 and populate posgres

---
+++ functions.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import connect
+from datetime import datetime
+from dateutil.rrule import rrule, DAILY
+
+
+def convert_response_to_df(response):
+    """
+    used for each iteration against the api, converts response to a dataframe
+    """
+
+    list = []
+
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+        rows = report.get('data', {}).get('rows', [])
+        sampled = True if report.get('samplesReadCounts') else False
+
+        for row in rows:
+            dict = {}
+            dict['sampling'] = sampled
+            dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
+
+            for header, dimension in zip(dimensionHeaders, dimensions):
+              dict[header] = dimension
+
+            for i, values in enumerate(dateRangeValues):
+                for metric, value in zip(metricHeaders, values.get('values')):
+                    if ',' in value or '.' in value:
+                        dict[metric.get('name')] = float(value)
+                    else:
+                      dict[metric.get('name')] = int(value)
+            list.append(dict)
+
+        df = pd.DataFrame(list)
+        return df
+
+
+def get_report(start_date, end_date, view_id, metrics, dimensions, dimensionFilterClauses=[], segments=[], pageToken=None):
+    """
+    This is the function that actually hits the api
+    """
+
+    return connect.service.reports().batchGet(
+        body={
+            'reportRequests': [{
+              'viewId': view_id,
+              'dateRanges': [{'startDate':start_date, 'endDate': end_date}],
+              'metrics': metrics,
+              'dimensions': dimensions,
+              'pageSize': 10000,
+              'dimensionFilterClauses': dimensionFilterClauses,
+              'segments': segments,
+              'pageToken': pageToken
+            }]
+          }
+      ).execute()
+
+
+def return_ga_data(start_date, end_date, view_id, metrics, dimensions, group_by=[], dimensionFilterClauses=[], segments=[]):
+    """
+    handles paginated query results and loops over input date range by day to return all results and minimise sampling
+    """
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+    final_list = []
+    for date in rrule(freq=DAILY, dtstart=start_date, until=end_date):
+        date = str(date.date())
+
+        # for paginated results
+        page_token = '0'  # for initial iteration this just needs to be anything except None
+        while page_token != None:
+            iresponse = get_report(date, date, view_id, metrics, dimensions, dimensionFilterClauses, segments, pageToken=page_token)
+            i_df = convert_response_to_df(iresponse)
+            final_list.append(i_df)
+            page_token = iresponse['reports'][0].get('nextPageToken')  # update the pageToken
+
+    final_df = pd.concat(final_list)
+    if len(group_by) != 0:
+        final_df = final_df.groupby(group_by).sum().reset_index()
+    return final_df
---
+++ run_sessions.py
@@ -0,0 +1,175 @@
+# packages and globals
+from functions import return_ga_data
+import credentials as creds
+import psycopg2
+from sqlalchemy import create_engine
+
+VIEW_ID = '197744684' # PB prod
+
+## sessions
+start_date = '2019-09-01'
+end_date = '2019-09-30'
+
+
+# common session dims and all metrics
+sessions1 = return_ga_data(
+  start_date = start_date,
+  end_date = end_date,
+  view_id = VIEW_ID,
+  metrics = [
+    {'expression': 'ga:sessions'},
+    {'expression': 'ga:bounces'},
+    {'expression': 'ga:sessionDuration'},
+    {'expression': 'ga:pageViews'},
+    {'expression': 'ga:goal1completions'},
+    {'expression': 'ga:goal2completions'},
+    {'expression': 'ga:transactions'},
+    {'expression': 'ga:transactionRevenue'}
+  ],
+  dimensions = [
+    {'name': 'ga:dimension1'}, # session ID
+    {'name': 'ga:dimension2'}, # client ID
+    {'name': 'ga:date'},
+    {'name': 'ga:hour'},
+    {'name': 'ga:minute'},
+    {'name': 'ga:landingPagePath'},
+    {'name': 'ga:deviceCategory'}
+  ],
+  group_by = ['ga:dimension1', 'ga:dimension2', 'ga:date', 'ga:hour', 'ga:minute',
+              'ga:landingPagePath', 'ga:deviceCategory'],
+  dimensionFilterClauses = [],
+  segments=[]
+)
+
+
+## continue with common dimensions
+sessions2 = return_ga_data(
+  start_date = start_date,
+  end_date = end_date,
+  view_id = VIEW_ID,
+  metrics = [
+    {'expression': 'ga:sessions'}
+  ],
+  dimensions = [
+    {'name': 'ga:dimension1'}, # session id
+    {'name': 'ga:userType'},
+    {'name': 'ga:operatingSystem'},
+    {'name': 'ga:operatingSystemVersion'}
+  ],
+  group_by = ['ga:dimension1', 'ga:userType', 'ga:operatingSystem', 'ga:operatingSystemVersion'],
+  dimensionFilterClauses = [],
+  segments=[]
+)
+
+
+## channel and source data
+sessions3 = return_ga_data(
+  start_date = start_date,
+  end_date = end_date,
+  view_id = VIEW_ID,
+  metrics = [
+    {'expression': 'ga:sessions'}
+  ],
+  dimensions = [
+    {'name': 'ga:dimension1'}, # session id
+    {'name': 'ga:source'},
+    {'name': 'ga:medium'},
+    {'name': 'ga:campaign'},
+    {'name': 'ga:adContent'},
+    {'name': 'ga:keyword'},
+    {'name': 'ga:channelGrouping'}
+  ],
+  group_by = ['ga:dimension1', 'ga:source', 'ga:medium', 'ga:campaign',
+              'ga:adContent', 'ga:keyword', 'ga:channelGrouping'],
+  dimensionFilterClauses = [],
+  segments=[]
+)
+
+# join
+## for sampling flag use result from sessions1. Same time frame so result will be the same regardless
+sessions_combined = sessions1.merge(sessions2.drop(['ga:sessions', 'sampling'], axis = 1),
+                                   on = 'ga:dimension1',
+                                   how = 'inner').merge(sessions3.drop(['ga:sessions', 'sampling'], axis = 1),
+                                                       on = 'ga:dimension1',
+                                                       how = 'inner')
+
+# dedup sessions. Pulling in hour and minute seems to cause duplicates, filtering out where sessions = 0 seems to match GA UI
+sessions_combined = sessions_combined[sessions_combined['ga:sessions'] > 0]
+
+## Get fields in order
+sessions_ordered = ['ga:dimension1',
+                    'ga:dimension2',
+                    'ga:date',
+                    'ga:hour',
+                    'ga:minute',
+                    'ga:userType',
+                    'ga:deviceCategory',
+                    'ga:operatingSystem',
+                    'ga:operatingSystemVersion',
+                    'ga:landingPagePath',
+                    'ga:source',
+                    'ga:medium',
+                    'ga:campaign',
+                    'ga:adContent',
+                    'ga:keyword',
+                    'ga:channelGrouping',
+                    'ga:sessions',
+                    'ga:bounces',
+                    'ga:sessionDuration',
+                    'ga:pageViews',
+                    'ga:goal1completions',
+                    'ga:goal2completions',
+                    'ga:transactions',
+                    'ga:transactionRevenue',
+                    'sampling']
+
+sessions_combined = sessions_combined[sessions_ordered]
+
+nice_names = ['dimension1',
+              'dimension2',
+              'date',
+              'hour',
+              'minute',
+              'user_type',
+              'device_category',
+              'operating_system',
+              'operating_system_version',
+              'landing_page_path',
+              'source',
+              'medium',
+              'campaign',
+              'ad_content',
+              'keyword',
+              'channel_grouping',
+              'sessions',
+              'bounces',
+              'session_duration',
+              'pageviews',
+              'goal1_completions',
+              'goal2_completions',
+              'transactions',
+              'transaction_revenue',
+              'sampling']
+
+sessions_combined.columns = nice_names
+
+# Clear memory. Might not be necessary but still haunted from using R
+del sessions1, sessions2, sessions3, nice_names, sessions_ordered
+
+
+# Postgres
+engine = create_engine('postgresql://' +
+                       creds.user + ':' +
+                       creds.pw + '@' +
+                       creds.host + ':' +
+                       creds.port + '/' +
+                       creds.db,
+                       echo=False)
+
+# post to sessions table
+sessions_combined.to_sql('sessions',
+                         con = engine,
+                         schema = 'google_analytics',
+                         index = False,
+                         if_exists = 'append')
+
